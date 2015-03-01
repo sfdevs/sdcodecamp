740e4458bce91769241b722bd85d2593ea7a2094
Tyler Harms <tyler.harms@gmail.com>
2015-02-28 23:25:17 -0600

    alternative approach to getting parameters to work on heroku

---
+++ app/config/parameters.php
@@ -0,0 +1,13 @@
+<?php
+
+if (isset($_ENV['DATABASE_URL'])) {
+    $dbUrl = $_ENV['DATABASE_URL'];
+    $parts = parse_url($dbUrl);
+
+    $container->setParameter('database.driver', 'pdo_pgsql');
+    $container->setParameter('database.host', $parts['host']);
+    $container->setParameter('database.name', trim($parts['path'], '/'));
+    $container->setParameter('database.user', $parts['user']);
+    $container->setParameter('database.password', $parts['pass']);
+    $container->setParameter('database.port', $parts['port']);
+}
