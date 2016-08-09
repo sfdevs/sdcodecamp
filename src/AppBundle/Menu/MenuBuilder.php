3201eaacc56aa7a8c9e3cb017dd93380115fcf11
Tyler Harms <tyler.harms@gmail.com>
2016-08-09 15:00:37 -0500

    remove schedule from menu for now

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -3,15 +3,19 @@
 namespace AppBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
-use Knp\Menu\Matcher\Matcher;
-use Knp\Menu\Matcher\Voter\RegexVoter;
 use Knp\Menu\Matcher\Voter\RouteVoter;
-use Knp\Menu\Matcher\Voter\UriVoter;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 
+/**
+ * Class MenuBuilder
+ * @package AppBundle\Menu
+ */
 class MenuBuilder
 {
+    /**
+     * @var FactoryInterface
+     */
     private $factory;
 
     /**
@@ -22,25 +26,28 @@ class MenuBuilder
         $this->factory = $factory;
     }
 
+    /**
+     * @param RequestStack $requestStack
+     * 
+     * @return \Knp\Menu\ItemInterface
+     */
     public function createMainMenu(RequestStack $requestStack)
     {
         $currentRequest = $requestStack->getCurrentRequest();
-        $menu = $this->factory->createItem('root')->setChildrenAttributes(array('class'=>'main', 'id'=>'main_nav'));
+        $menu = $this->factory->createItem('root')->setChildrenAttributes(array('class' => 'main', 'id' => 'main_nav'));
 
         //        $menu->addChild('Home', array('route' => 'homepage'));
-        $menu->addChild('Speakers', array('route'=> 'speaker_index'));
-        $menu->addChild('Sessions', array('route'=> 'session_index'));
-        $menu->addChild('Sponsors', array('route'=> 'page_sponsors'));
-        $menu->addChild('Schedule', array('route'=> 'page_schedule'));
-        $menu->addChild('Venue', array('route'=> 'page_venue'));
+        $menu->addChild('Speakers', array('route' => 'speaker_index'));
+        $menu->addChild('Sessions', array('route' => 'session_index'));
+        $menu->addChild('Sponsors', array('route' => 'page_sponsors'));
+//        $menu->addChild('Schedule', array('route'=> 'page_schedule'));
+        $menu->addChild('Venue', array('route' => 'page_venue'));
 
         $RouteVoter = new RouteVoter();
         $RouteVoter->setRequest($currentRequest);
 
-        foreach ($menu->getChildren() as $menuItem)
-        {
-            if ($this->isAncestor($currentRequest->getUri(), $menuItem->getUri()))
-            {
+        foreach ($menu->getChildren() as $menuItem) {
+            if ($this->isAncestor($currentRequest->getUri(), $menuItem->getUri())) {
                 $menuItem->setCurrent(true);
             }
 
